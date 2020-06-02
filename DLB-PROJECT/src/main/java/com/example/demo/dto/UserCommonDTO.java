5aa558353537cc29e24bd317d3573afb3fff8efc
sivaprasadguptha <siva.p.guptha@in.ey.com>
2020-06-02 10:01:13 +0530

    added dto class

---
+++ DLB-PROJECT/src/main/java/com/example/demo/dto/UserCommonDTO.java
@@ -0,0 +1,140 @@
+package com.example.demo.dto;
+/**
+ * This class is to carry data across multiple classes
+ * @author Dipak
+ *
+ */
+public class UserCommonDTO {
+
+	private String userName;
+	private String name;
+	private String password;
+	private Long phoneNumber;
+	private String email;
+	private Integer otp;
+	private String newPAssword;
+	private String oldPassword;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getOtp() {
+		return otp;
+	}
+	public void setOtp(Integer otp) {
+		this.otp = otp;
+	}
+	public String getNewPAssword() {
+		return newPAssword;
+	}
+	public void setNewPAssword(String newPAssword) {
+		this.newPAssword = newPAssword;
+	}
+	public String getOldPassword() {
+		return oldPassword;
+	}
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((newPAssword == null) ? 0 : newPAssword.hashCode());
+		result = prime * result + ((oldPassword == null) ? 0 : oldPassword.hashCode());
+		result = prime * result + ((otp == null) ? 0 : otp.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserCommonDTO other = (UserCommonDTO) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (newPAssword == null) {
+			if (other.newPAssword != null)
+				return false;
+		} else if (!newPAssword.equals(other.newPAssword))
+			return false;
+		if (oldPassword == null) {
+			if (other.oldPassword != null)
+				return false;
+		} else if (!oldPassword.equals(other.oldPassword))
+			return false;
+		if (otp == null) {
+			if (other.otp != null)
+				return false;
+		} else if (!otp.equals(other.otp))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phoneNumber == null) {
+			if (other.phoneNumber != null)
+				return false;
+		} else if (!phoneNumber.equals(other.phoneNumber))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "UserCommonDTO [userName=" + userName + ", name=" + name + ", password=" + password + ", phoneNumber="
+				+ phoneNumber + ", email=" + email + ", otp=" + otp + ", newPAssword=" + newPAssword + ", oldPassword="
+				+ oldPassword + "]";
+	}
+	
+}
