5e58cffb5175433f66c5b767d305d7e0e3d7b453
Windows <Windows@DESKTOP-ON92HN9>
2020-06-17 11:47:27 +0530

    a

--- DLB-PROJECT/src/main/java/com/dlb/controller/UserController.java
+++ DLB-PROJECT/src/main/java/com/dlb/controller/UserController.java
@@ -3,14 +3,15 @@ package com.dlb.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +20,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.dlb.entity.UserEntity;
 import com.dlb.model.UserDomain;
 import com.dlb.service.UserServiceImpl;
-import com.sun.xml.bind.v2.TODO;
 
 @Controller
 public class UserController {
@@ -80,7 +80,7 @@ public class UserController {
 	 */
 	@RequestMapping(value = "/signupPost", method = RequestMethod.POST)
 	public String storeUserdata(Model model, @RequestParam("file") MultipartFile imageFile,
-			@ModelAttribute(name = "signUpdomain") UserDomain domain) {
+			@Valid@ModelAttribute(name = "signUpdomain") UserDomain domain, BindingResult result) {
 		System.out.println("UserController.storeUserdata()");
 		UserEntity userid = service.createUserAccount(domain, imageFile);
 		if (userid != null) {
