efd874ca431e5c7e6c4537e20667df93df00e99a
dipak kumar saha <dsaha8598@gmail.com>
2021-08-06 16:07:33 +0530

    Added admin project

---
+++ Udajahaja/udajahaja-admin/src/main/java/com/udajahaja/ZullApp1Application.java
@@ -0,0 +1,13 @@
+package com.udajahaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZullApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZullApp1Application.class, args);
+	}
+
+}
